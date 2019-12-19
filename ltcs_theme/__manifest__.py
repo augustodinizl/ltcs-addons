32666830fbf4668c00110cdff83e2c6a5e5ca966
Augusto D. Lisbôa <augusto.diniz.l@gmail.com>
2019-12-19 13:55:19 -0300

    1

---
+++ ltcs_theme/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  'name':'LTCS Website Theme',
+  'description': 'Tema para o site da LTech.',
+  'version':'1.0',
+  'author':'Augusto D. Lisbôa',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
