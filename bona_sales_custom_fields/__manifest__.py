382fb21957c8818da20eced1ad6593a7b84f4b77
Augusto Lisbôa <augustolisboa@MacBook-de-Ltech.local>
2020-08-08 10:41:27 -0300

    update v12

---
+++ bona_sales_custom_fields/__manifest__.py
@@ -0,0 +1,19 @@
+{ 
+    'name': "Campos customizados para ordem de venda / cotação - Bonamaison",
+
+    'summary': """""",
+
+    'description': """""",
+    'author': "LTech - Consulting Services",
+    'website': "http://www.ltcs.com.br",
+    'category': 'Uncategorized',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Augusto Lisbôa <augusto@ltcs.com.br>',
+    ],
+    'depends': ['br_sale'],
+    'data': [
+        'views/sale_order.xml',
+    ],
+}
