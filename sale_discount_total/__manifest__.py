5e3c88892a1b107e336ffdec6697a46c679e7f8c
Augusto D. Lisbôa <augusto.diniz.l@gmail.com>
2020-08-29 11:03:03 -0300

    Update __manifest__.py

--- sale_discount_total/__manifest__.py
+++ sale_discount_total/__manifest__.py
@@ -12,6 +12,8 @@
         Desconto sobre o total da venda, o desconto pode ser do tipo
         percentual ou uma quantia específica para ser descontada do
         total bruto da venda.
+        Inspirado no módulo: https://github.com/CybroOdoo/CybroAddons/tree/12.0/sale_discount_total
+        Com ajustes para solução de dízima gerada no rateio do desconto total em montante monetário para mais de 2 itens na sale.order.line
     """,
     'author': "LTCS",
     'website': "http://www.ltcs.com.br",
